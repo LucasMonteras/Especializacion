24534126071d5af5eef2a89297f943ff74dd0d86
LucasMonteras <lucasmonteras@gmail.com>
2024-01-18 22:59:17 -0300

    Mensaje de confirmación

--- Microservicios/Practicas Modulo 3/Eureka-server/checkout-service/src/main/java/com/example/Checkoutservice/service/IProductService.java
+++
@@ -1,8 +0,0 @@
-package com.example.Checkoutservice.service;
-
-import com.example.Checkoutservice.model.dto.Product;
-
-public interface IProductService {
-
-    public Product getProduct(String id);
-}
